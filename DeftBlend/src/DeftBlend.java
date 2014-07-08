3f6681b31593e2636c111709cd51ff3591b2e30e
pugh-jeremy <pugh-jeremy@users.noreply.github.com>
2014-07-07 19:05:54 -0500

    Created some stub methods to handle phase1 POC for quick parsing of job posts and indentification of matching resume content.
    Signed-off-by: pugh-jeremy <pugh-jeremy@users.noreply.github.com>

--- DeftBlend/src/DeftBlend.java
+++ DeftBlend/src/DeftBlend.java
@@ -2,5 +2,22 @@ public class DeftBlend {
   public static void main(String args[]) {
 
   } //ends main method
+  
+  public String getContent (String URL) {
+     return URL;
+  } //ends the getContent method
+
+  public String generateTags (String content) {
+    return content;
+  } //ends the generateTags method
+
+  public void storeTags (String[] tags) {
+    
+  } //ends the storeTags method
+
+  public String[] generateStudyList (String[] tagListA, String[] tagListB) {
+    String[] studyList = null;
+    return studyList;
+  } //ends the generateStudyList method 
 
 }//ends DeftBlend class
